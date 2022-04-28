78c1fda7232762c6788c539468cce6e2b8d56f77
Yassar Ahmed Chohan <yassar.ahmed@systemsltd.com>
2022-04-28 15:32:23 +0500

    api-gateway

---
+++ src/test/java/com/etisalat/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etisalat.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
