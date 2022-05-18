74b8aee66a9db706ee49fa4bec35e520bcaad725
razamd12 <razamuhammad.dahri@systemsltd.com>
2022-05-18 19:53:59 +0500

    add

---
+++ src/main/java/com/etisalat/apigateway/exception/JwtTokenMalformedException.java
@@ -0,0 +1,13 @@
+package com.etisalat.apigateway.exception;
+
+import javax.naming.AuthenticationException;
+
+public class JwtTokenMalformedException extends AuthenticationException {
+
+	private static final long serialVersionUID = 1L;
+
+	public JwtTokenMalformedException(String msg) {
+		super(msg);
+	}
+
+}
